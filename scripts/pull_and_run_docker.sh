46dc5fedf25fe0940a641d648e156721350e2b50
Madhan <Madhan@host.docker.internal>
2023-11-30 14:46:22 +0530

    again now

--- scripts/pull_and_run_docker.sh
+++ scripts/pull_and_run_docker.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Stop and remove existing Docker container
-sudo docker stop ${{ env.DOCKER_CONTAINER_NAME }} || true
-sudo docker rm ${{ env.DOCKER_CONTAINER_NAME }} || true
+sudo docker stop shoumtech || true
+sudo docker rm shoumtech || true
 
 #Login to pull docker image
-sudo docker login -u ${{ secrets.DOCKERHUB_USERNAME }} -p ${{ secrets.DOCKERHUB_TOKEN }}
+sudo docker login -u shoumtech -p dckr_pat_H9hK8c-zxAHkMFHN9u_vjmkEbvw
 
 # Pull Docker image from Docker Hub
-sudo docker pull ${{ secrets.DOCKERHUB_USERNAME }}/${{ env.DOCKER_IMAGE }}
+sudo docker pull shoumtech/testpipe_v1:latest
 
 # Run Docker container
-sudo docker run -d --name ${{ env.DOCKER_CONTAINER_NAME }} -p 8081:8080 ${{ secrets.DOCKERHUB_USERNAME }}/${{ env.DOCKER_IMAGE }}
+sudo docker run -d --name shoumtech -p 8081:8080 shoumtech/testpipe_v1:latest
